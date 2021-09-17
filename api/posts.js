1619e3b48a23af557864e046b3591b1c60a347e4
Leslie Bradford <lesliebradford@Leslies-MacBook-Air.local>
2021-09-17 14:56:37 -0300

    end of day 1

---
+++ api/posts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { getAllPosts } = require('../db');
+const postsRouter = express.Router();
+
+postsRouter.use((req, res, next) => {
+    console.log("A request is being made to /posts");
+
+    next(); // THIS IS DIFFERENT
+});
+
+postsRouter.get('/', async (req, res) => {
+    const posts = await getAllPosts();
+
+    res.send({ posts });
+});
+
+module.exports = postsRouter;
